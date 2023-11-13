85460d8b831d5c6134e6fbf410742d99d869f36e
artsiom <artsiomzhuchkou2401@gmail.com>
2023-11-13 14:45:04 +0300

    edit test

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,3 @@
 import generate from '../src/index';
 
-generate({}, 'master-detail.xlsx')
+generate({name: 'Name', assets: [{serialNumber: 1, jobs: [{title: 'title1', startDate: '2022-12-12', completionDate: '2022-12-14'}, {title: 'title2', startDate: '2022-12-12', completionDate: '2022-12-14'}]}, {serialNumber: 2, jobs: [{title: 'title1', startDate: '2022-12-12', completionDate: '2022-12-14'}, {title: 'title2', startDate: '2022-12-12', completionDate: '2022-12-14'}]}, {serialNumber: 3, jobs: [{title: 'title1', startDate: '2022-12-10', completionDate: '2022-12-14'}]}]}, 'master-detail.xlsx')
