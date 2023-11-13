967bcd96a33127ddf3444eba07eefe09962a5a7a
artsiom <artsiomzhuchkou2401@gmail.com>
2023-11-13 14:44:27 +0300

    feat: added types and remove unused logic

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,45 @@
-export interface IComplexVariable {entityName: string, fieldName: string, type: string, address: string}
+export interface IMaster {
+  entityName: string,
+  fieldName: string,
+  address: string,
+  type: string
+  alignment: IAlignment
+}
+
+export interface IFormula {
+  formula: string;
+  address: string;
+  alignment: IAlignment
+}
+
+export interface IFormulas {
+  rowFormulas: IFormula[];
+  columnFormulas: IFormula[];
+}
+
+interface IAlignment {
+  horizontal: string, vertical: string
+}
+
+interface IStaticVariable {
+  value: string;
+  address: string;
+  alignment: IAlignment
+}
+
+export interface IStaticVariables {
+  [key: string]: IStaticVariable;
+}
+
+
+export interface IDetail {
+  entityName: string;
+  fieldName: string;
+  address: string;
+  type: string;
+  alignment: IAlignment
+}
+
+export interface IDetails {
+  [key: string]: IDetail[];
+}
