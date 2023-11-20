0da28b9132057ae9b00cc6ce6d6f69dcb0673a97
artsiom <artsiomzhuchkou2401@gmail.com>
2023-11-20 14:53:45 +0300

    feat: added copy diagramm

--- src/Templater.ts
+++ src/Templater.ts
@@ -1,18 +1,25 @@
 import path from 'path';
 import {writeFile} from 'fs/promises';
 import Workbook from "exceljs/index";
+// @ts-ignore
 import excel from 'exceljs';
+// @ts-ignore
+import copyExcelChart from './copy-excel-chart/copy-excel-chart'
+
+const readCharts = copyExcelChart.readCharts
+const copyChart = copyExcelChart.copyChart
+const writeCharts = copyExcelChart.writeCharts
 import {
     getSimpleVariable,
     getComplexVariable,
     getFormula,
     parseIntFromString,
     parseLetterFromString,
-    isItRowFormula, replaceSpecificNumberInFormula, addDifferenceToTheLastNumber
+    isItRowFormula, replaceSpecificNumberInFormula, addDifferenceToTheLastNumber, extendRange
 } from "./Parser";
 import {IDetail, IMaster, IDetails, IFormulas, IStaticVariables, ISimpleVariables} from "./types";
 
-const cellFont = { name: 'Arial', size: 11 };
+const cellFont = {name: 'Arial', size: 11};
 
 const fs = require('fs').promises;
 
@@ -33,24 +40,54 @@ const createArrayIfNotExist = (list: any, entity: string) => {
     }
 }
 
+const copyDiagramm = async (template: string, report: string, length: number) => {
+    const source = await readCharts(template, './temp')
+    const output = await readCharts(report, './temp')
+    const summary = source.summary();
+    let replaceCellRefs = summary['template']['chart1'].reduce((acc: any, el: any) => {
+        return {...acc, [el]: el.replace('recommendWorksheet2', 'worksheet-Recommendation')}
+    }, {})
+
+
+    for (let key in replaceCellRefs) {
+        replaceCellRefs[key] = extendRange(replaceCellRefs[key], length)
+    }
+    copyChart(
+        source,
+        output,
+        'template',
+        'chart1',
+        'template',
+        replaceCellRefs,
+    )
+
+    writeCharts(output, report)
+}
+
 async function parse(worksheet: Workbook.Worksheet) {
     try {
         const simpleVariables: any = {}
         let master: any = null;
         const details: any = {};
-        const formulas: any = {rowFormulas: [], columnFormulas: []};
+        const formulas: IFormulas = {rowFormulas: [], columnFormulas: [], masterFormulas: []};
         const staticVariables: any = {};
-        worksheet?.eachRow((row: Workbook.Worksheet.row, rowNumber: Workbook.Worksheet.rowNumber) => {
+        let masterRowNumber: number = -1;
+        worksheet?.eachRow((row: Workbook.Worksheet.row, rowNumber: number) => {
             row.eachCell((cell: Workbook.Worksheet.cell, colNumber: Workbook.Worksheet.colNumber) => {
                 const simpleVariable = getSimpleVariable(cell);
                 if (simpleVariable) {
                     createArrayIfNotExist(simpleVariables, simpleVariable.variable)
-                    simpleVariables[simpleVariable.variable].push({address: simpleVariable.address, alignment: cell.alignment, variable: simpleVariable.variable})
+                    simpleVariables[simpleVariable.variable].push({
+                        address: simpleVariable.address,
+                        alignment: cell.alignment,
+                        variable: simpleVariable.variable
+                    })
                 }
                 const complexVariable: IDetail | null = getComplexVariable(cell);
                 if (complexVariable) {
                     if (complexVariable.type === 'master') {
                         master = complexVariable
+                        masterRowNumber = rowNumber
                     }
 
                     if (complexVariable.type === 'detail') {
@@ -62,27 +99,40 @@ async function parse(worksheet: Workbook.Worksheet) {
 
                 if (formula) {
                     if (isItRowFormula(formula.formula)) {
-                        formulas.rowFormulas.push(formula);
+                        if (masterRowNumber === rowNumber) {
+                            formulas.masterFormulas.push(formula)
+                        } else {
+                            formulas.rowFormulas.push(formula);
+                        }
                     } else {
                         formulas.columnFormulas.push(formula);
                     }
                 }
 
                 if (!simpleVariable && !complexVariable && !formula) {
-                    staticVariables[cell.address] =  {value: cell.value, address: cell.address, alignment: cell.alignment}
+                    staticVariables[cell.address] = {
+                        value: cell.value,
+                        address: cell.address,
+                        alignment: cell.alignment
+                    }
                 }
             });
         });
         return {simpleVariables, master, details, formulas, staticVariables}
     } catch (err) {
         console.error('Error parse the file:', err);
-        return {simpleVariables: {}, master: {}, details: {}, formulas: [], staticVariables: {}}
+        return {
+            simpleVariables: {},
+            master: {},
+            details: {},
+            formulas: {rowFormulas: [], columnFormulas: [], masterFormulas: []},
+            staticVariables: {}
+        }
     }
 }
 
 async function putMasterDetail(worksheet: Workbook.Worksheet, master: IMaster, details: IDetails, data: any, formulas: IFormulas, staticVariables: IStaticVariables) {
     try {
-        console.log(formulas, 'formulas')
         const masterDetailDeep = 1;
         let currentRowNumber: number = parseIntFromString(master.address);
         const startRowNumber: number = currentRowNumber;
@@ -95,6 +145,22 @@ async function putMasterDetail(worksheet: Workbook.Worksheet, master: IMaster, d
             cell.value = masterEntity[master.fieldName];
             cell.font = cellFont;
             cell.alignment = master.alignment;
+
+            // put master formula
+            if (formulas.masterFormulas.length) {
+                const masterFormulaColumn: string | null = parseLetterFromString(formulas.masterFormulas[0].address);
+                if (masterFormulaColumn) {
+                    formulas.masterFormulas.forEach((masterFormula) => {
+                        const cell = worksheet.getCell(masterFormulaColumn + currentRowNumber);
+                        cell.value = {formula: `SUM(G${currentRowNumber + 1}:G${currentRowNumber + masterEntity[detailsKeys[0]].length})`};
+                        cell.font = cellFont;
+                        cell.alignment = masterFormula.alignment;
+                    })
+                }
+
+            }
+
+
             currentRowNumber += 1;
             worksheet.spliceRows(currentRowNumber + masterDetailDeep, 0, []);
             // put details row
@@ -139,12 +205,12 @@ async function putMasterDetail(worksheet: Workbook.Worksheet, master: IMaster, d
         // put column formulas
         const difference = currentRowNumber - startRowNumber;
         formulas.columnFormulas.forEach((formula) => {
-                    console.log(formula, 'formula')
-                    const originalRowNumber = parseIntFromString(formula.address);
-                    const formulaCell = worksheet.getCell(replaceSpecificNumberInFormula(formula.address, originalRowNumber, originalRowNumber + difference));
-                    formulaCell.value = {formula: addDifferenceToTheLastNumber(formula.formula, difference)};
-                    formulaCell.alignment = formula.alignment;
-                })
+            const originalRowNumber = parseIntFromString(formula.address);
+            const formulaCell = worksheet.getCell(replaceSpecificNumberInFormula(formula.address, originalRowNumber, originalRowNumber + difference));
+            formulaCell.value = {formula: addDifferenceToTheLastNumber(formula.formula, difference)};
+            formulaCell.alignment = formula.alignment;
+        })
+        return currentRowNumber - startRowNumber;
     } catch (err) {
         console.error('Error put master detail to the file:', err);
         return null;
@@ -156,7 +222,7 @@ async function putSimpleVariables(worksheet: Workbook.Worksheet, data: any, simp
         for (let variable in simpleVariables) {
             if (data[variable]) {
                 simpleVariables[variable].forEach((simpleVariable) => {
-                   const variableCell = worksheet.getCell(simpleVariable.address);
+                    const variableCell = worksheet.getCell(simpleVariable.address);
                     variableCell.value = data[variable];
                     variableCell.alignment = simpleVariable.alignment;
                 })
@@ -171,15 +237,18 @@ async function putSimpleVariables(worksheet: Workbook.Worksheet, data: any, simp
 const buildTemplate = async (dataToFill: {}, path: string) => {
     const {workbook, workSheet}: any = await readFile(path);
     const {master, details, simpleVariables, formulas, staticVariables} = await parse(workSheet);
+
     const masterTyped: IMaster = master;
     const detailsTyped: IDetails = details;
     // put simple variables
     putSimpleVariables(workSheet, dataToFill, simpleVariables)
 
     // put master-details
-    await putMasterDetail(workSheet, masterTyped, detailsTyped, dataToFill, formulas, staticVariables)
-
-    await workbook.xlsx.writeFile('report.xlsx');
+    const lenght: number | null = await putMasterDetail(workSheet, masterTyped, detailsTyped, dataToFill, formulas, staticVariables);
+    if (lenght) {
+        await workbook.xlsx.writeFile('report.xlsx');
+        copyDiagramm(path, './report.xlsx', lenght)
+    }
 
 
 }
