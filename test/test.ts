fcb7da5f402d37efd0f0946fd2f16045b56b7108
artsiom <artsiomzhuchkou2401@gmail.com>
2024-01-18 14:09:59 +0300

    refactor test files

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,12 @@
-import generate from 'xlsx-constructor';
-
-const data = {name: 'Name', Model: '2022-12-12', startDatePeriod: '2022-12-12', jobs: [{title: 'title1', startDate: '1657112642148', completionDate: '1657116642148'}, {title: 'title2', startDate: '2022-12-13', completionDate: '2022-12-15'}],  Assets: [{serialNumber: 1, jobs: [{title: 'title1', startDate: '2022-12-12', completionDate: '2022-12-14'}, {title: 'title2', startDate: '2022-12-12', completionDate: '2022-12-14'}]}, {serialNumber: 2, jobs: [{title: 'title1', startDate: '2022-12-12', completionDate: '2022-12-14'}, {title: 'title2', startDate: '2022-12-12', completionDate: '2022-12-14'}]}, {serialNumber: 3, jobs: [{title: 'title1', startDate: '2022-12-10', completionDate: '2022-12-14'}]}]}
-const templatePath = 'templates/without-diagramm.xlsx';
+import generate from '../src/index';
+const fs = require('fs').promises;
+const templatePath = 'templates/with-diagram.xlsx';
 const reportPath = 'report.xlsx';
 const temporaryFolder = 'temporary';
-generate(data, templatePath, reportPath, temporaryFolder)
+const start = async () => {
+  const jsonData = await fs.readFile('assetClass.json', 'utf8');
+  const parsedData = JSON.parse(jsonData);
+  generate(parsedData, templatePath, reportPath, temporaryFolder);
+};
+start();
 // generate({name: 'Name', startDatePeriod: '2022-12-12', assets: [{serialNumber: 1, jobs: [{title: 'title1', startDate: '2022-12-12', completionDate: '2022-12-14'}, {title: 'title2', startDate: '2022-12-12', completionDate: '2022-12-14'}]}, {serialNumber: 2, jobs: [{title: 'title1', startDate: '2022-12-12', completionDate: '2022-12-14'}, {title: 'title2', startDate: '2022-12-12', completionDate: '2022-12-14'}]}, {serialNumber: 3, jobs: [{title: 'title1', startDate: '2022-12-10', completionDate: '2022-12-14'}]}]}, 'with-diagramm.xlsx')
