3ef0a9d9947745896cce0336df0a6d8535141a58
artsiom <artsiomzhuchkou2401@gmail.com>
2024-03-11 11:57:46 +0300

    added parse and readFile types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,3 @@
 export default function generate(data: any, templatePath: string, reportPath: string, tempFolderPath: string): void {}
+export function readFile(path: string): void {}
+export function parse(worksheet: Workbook.Worksheet): void {}
